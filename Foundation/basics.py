4939f8dc8de995359dd9f618f0444a4541dd3b99
Nagaraju Yaragadla <nagaraju.yaragadla@oracle.com>
2025-08-27 19:33:42 +0530

    type evaluation

--- Foundation/basics.py
+++ Foundation/basics.py
@@ -1,2 +1,16 @@
 name = input('Enter Name : ')
 print(f'Welcome to python Mr.{name}')
+
+print('---------------------')
+
+x = 123
+print(f'x : {x}')
+print(f'x type of {type(x)}')
+
+print('---------------------')
+
+y = 123.345
+print(f'y : {y}')
+print(f'y of type {type(y)}')
+
+print('---------------------')
